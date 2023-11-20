4d5300b8d2f0caa61b63e9e015dc62336d33b8b4
nikhilgit02 <srivastavanikhil107@gmail.com>
2023-11-20 22:27:54 +0530

    added new meme component

---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme(){
+    return(
+        <section>
+            <form>
+            <input type="text" placeholder="Top Text"/>
+            <input type="text" placeholder="Bottom Text"/>
+            </form>
+            <button>Get a new meme image  🖼</button>
+        </section>
+    )
+}
